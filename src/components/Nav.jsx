d994dbbd3a7005f8c1910605ba2a72896e43abdd
kalungirasuli <kalungirasuli495@gmail.com>
2023-08-06 18:00:21 +0300

    dashboard

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+
+
+function Nav() {
+  return (
+    <>
+      <div className="nav">
+        <ul>
+            <li className="logo">SCHOOL</li>
+            <li>Home</li>
+            <li>Staff</li>
+            <li>Students</li>
+            <li>Clubs</li>
+            <li>Departments</li>
+            <li>Courses</li> 
+        </ul>
+      </div>
+    </>
+  )
+}
+export default Nav
