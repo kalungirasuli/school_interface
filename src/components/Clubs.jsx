e11255414f2debb036f8e3283187bb48d99c5ec2
Waguti Warren <warrenw639@gmail.com>
2023-08-07 09:18:41 +0300

    added config for clubs

---
+++ src/components/Clubs.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {useState, useEffect} from "react";
+
+function Clubs() {
+    const [clubData, setClubData] = useState([]);
+
+    const fetchClubs = () => {
+        //get data from the db
+        fetch('https://schoolapi-op58.onrender.com/v1/clubs/')
+            .then(response => response.json())
+            .then(data => {
+                setClubData(data);
+            })
+            .catch(error => {
+                console.log('Error:', error);
+            });
+    }
+
+    useEffect(() => {
+        fetchClubs();
+    }, []);
+
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Club</th>
+                    <th>Head of Club</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                {clubData.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.clubName}</td>
+                        <td>{item.headOfClub}</td>
+                        <td>
+                            <button className='edit'>Edit</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+export default Clubs;
