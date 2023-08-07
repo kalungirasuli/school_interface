baaaa92ed6734bb0ea7435ee5f4f142c80ca6da5
Johnson11 <kalijoe11@gmail.com>
2023-08-08 01:50:01 +0300

    stdents crud

--- src/components/students.jsx
+++ src/components/students.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
 
 function StudentTable() {
     const [students, setStudents] = useState([]);
