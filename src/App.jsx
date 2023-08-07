ff58d8d340b7e42a07e344127e1de1ed51634aa7
kalungirasuli <kalungirasuli495@gmail.com>
2023-08-07 07:56:35 +0300

    courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import React from 'react'
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
  //components
  import Nav from './components/Nav.jsx'
+ import Courses from './components/Courses.jsx'
 function App() {
  
 
@@ -9,8 +12,16 @@ function App() {
     <div className="main">
     <Nav />
     <div className="view">
-      <h3> TITLE</h3>
-      <hr />
+     {/* <BrowserRouter>
+      <Switch>
+          <Route path="/Staff">  */}
+            <h3> TITLE</h3>
+            <hr />
+            <Courses />
+          {/* </Route>
+        </Switch>
+      </BrowserRouter> */}
+      
     </div>
     </div>
      
